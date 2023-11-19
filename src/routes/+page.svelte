5b868d8d5b765334d8d1b3d7a06c32b376d7951f
Simeon Duwel <tameimpalafan123@gmail.com>
2023-11-19 23:10:54 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,126 @@
+<div class="w-screen min-h-screen bg-amber-100 p-32">
+    <h1 class="text-3xl mb-6 font-bold text-amber-900">
+        De Kempen zijn geen provincie.
+    </h1>
+    <p>
+        Het is een onherroepelijk feit: die miserabele zandstreek in Antwerpen
+        en Limburg is geen provincie. Ze hebben geen aparte provinciale klasse,
+        geen provinciaal wapenschild en je kunt er niet voor zuipen op een
+        zichzelf respecterende provinciezuip. Het woord "Kempenaar" duidt zowel
+        op een bewoner van dat misselijke gebied (en vaak zijn het ook
+        chauvinistische sujetten) als op een bepaald soort aardappel: dat zegt
+        genoeg, dunkt mij.
+    </p>
+    <h2 class="text-xl mb-3 font-bold text-amber-900">
+        Gemeenten die tot de Kempen behoren en bijgevolg minderwaardig zijn:
+    </h2>
+    <ul class="list-disc columns-4">
+        <li>Aalst</li>
+        <li>Arendonk</li>
+        <li>Baarle-Hertog</li>
+        <li>Baarle-Nassau</li>
+        <li>Baarschot</li>
+        <li>Balen</li>
+        <li>Beerse</li>
+        <li>Bergeijk</li>
+        <li>Berlaar</li>
+        <li>Best</li>
+        <li>Biest-Houtakker</li>
+        <li>Bladel</li>
+        <li>Bonheiden</li>
+        <li>Borkel en Schaft</li>
+        <li>Brasschaat</li>
+        <li>Brecht</li>
+        <li>Budel</li>
+        <li>Casteren</li>
+        <li>Dalem</li>
+        <li>Dessel</li>
+        <li>Diessen</li>
+        <li>Dommelen</li>
+        <li>Duffel</li>
+        <li>Duizel</li>
+        <li>Eersel</li>
+        <li>Eindhoven</li>
+        <li>Esbeek</li>
+        <li>Essen</li>
+        <li>Gastel</li>
+        <li>Geel</li>
+        <li>Geldrop</li>
+        <li>Gerwen</li>
+        <li>Goirle</li>
+        <li>Grobbendonk</li>
+        <li>Haghorst</li>
+        <li>Hapert</li>
+        <li>Heeze</li>
+        <li>Heist-op-den-Berg</li>
+        <li>Herentals</li>
+        <li>Herenthout</li>
+        <li>Herselt</li>
+        <li>Hilvarenbeek</li>
+        <li>Hooge en Lage Mierde</li>
+        <li>Hoogeloon</li>
+        <li>Hoogstraten</li>
+        <li>Hulsel</li>
+        <li>Hulshout</li>
+        <li>Kalmthout</li>
+        <li>Kapellen</li>
+        <li>Kasterlee</li>
+        <li>Knegsel</li>
+        <li>Laakdal</li>
+        <li>Leende</li>
+        <li>Lier</li>
+        <li>Lille</li>
+        <li>Luyksgestel</li>
+        <li>Maarheeze</li>
+        <li>Malle</li>
+        <li>Meerhout</li>
+        <li>Meerveldhoven</li>
+        <li>Merksplas</li>
+        <li>Middelbeers</li>
+        <li>Moergestel</li>
+        <li>Mol</li>
+        <li>Nederweert</li>
+        <li>Netersel</li>
+        <li>Nijlen</li>
+        <li>Nuenen</li>
+        <li>Oerle</li>
+        <li>Oirschot</li>
+        <li>Oisterwijk</li>
+        <li>Olen</li>
+        <li>Oostelbeers</li>
+        <li>Oud-Turnhout</li>
+        <li>Putte</li>
+        <li>Ranst</li>
+        <li>Ravels</li>
+        <li>Retie</li>
+        <li>Reusel</li>
+        <li>Riel</li>
+        <li>Riethoven</li>
+        <li>Rijkevorsel</li>
+        <li>Schilde</li>
+        <li>Schoten</li>
+        <li>Soerendonk</li>
+        <li>Spoordonk</li>
+        <li>Steensel</li>
+        <li>Sterksel</li>
+        <li>Turnhout</li>
+        <li>Valkenswaard</li>
+        <li>Veldhoven</li>
+        <li>Vessem</li>
+        <li>Vorselaar</li>
+        <li>Vosselaar</li>
+        <li>Waalre</li>
+        <li>Walik</li>
+        <li>Weebosch</li>
+        <li>Weert</li>
+        <li>Westelbeers</li>
+        <li>Westerhoven</li>
+        <li>Westerlo</li>
+        <li>Wijnegem</li>
+        <li>Wintelre</li>
+        <li>Wuustwezel</li>
+        <li>Zandhoven</li>
+        <li>Zeelst</li>
+        <li>Zoersel</li>
+    </ul>
+</div>
